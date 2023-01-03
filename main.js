38b13bbfd5c50b5f2183bf97fd9be0b583e006c8
layerunk <89783661+layerunk@users.noreply.github.com>
2023-01-03 12:49:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+robyer = 0;
+Xtremem = 0;
 function setup(){
     canvas = createCanvas(300 , 300);
     canvas.center()
@@ -11,22 +13,26 @@ function setup(){
   function depression(insecurity){
     if(insecurity.length > 0){
       console.log(insecurity);
-      console.log(insecurity[0].pose.nose.x + "X mas day");
-      console.log(insecurity[0].pose.nose.y + "Y Mr./Mrs ?");
+      robyer = insecurity[0].pose.nose.y - 5;
+      Xtremem = insecurity[0].pose.nose.x - 30 ;
+      console.log(Xtremem+ "Xmas instrument");
+      console.log(robyer + "whY ?");
     }
   }
   
   function consoler(){
-    console.log("Initialization will begin in 5, 4, 3, 2, 1 and ....");
+    console.log("Invasion will begin in 5, 4, 3, 2, 1 and ....");
   }
   
   function preload(){
+    hit = loadImage("https://i.postimg.cc/PJzrgz22/mostache.png");
   }
   
   function draw(){
     image(video, 0, 0, 300, 300);
+    image(hit, Xtremem, robyer, 63, 42);
   }
   
   function tapwater(){
-    save("cursedexistence.png")
+    save("cursedexistence.png");
   }
